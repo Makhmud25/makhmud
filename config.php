50f4cf2a3a08a97252b0f644b04153683c049a75
Makhmud25 <74974164+Makhmud25@users.noreply.github.com>
2020-11-24 21:44:14 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,44 @@
+},
+jangan lupa subscribe ,likw, komentar,& share
+yang udah download script mohon perhatian
+jangan main tarlalu lama nantik yang udah frofit 
+di ambil si jeke lagi 😂😂
+gunakan betset dengan bijak 
+
+resiko di tanggung pemenang
+salam frofit ,,,,
+donasi sekilas nya 
+  DNmb2ECyVQcBEZFiYYcS82n3rWuXVgpf37
+#yang mau aja😂😂😂
+
+betset,,,,
+ "Target Profit": "10",
+ "Target Win": "100000000",
+ "Lose Target": "0",
+ "Reset If Target Profit": "OFF",
+ "Reset If Target Win": "OFF",
+ "Interval": "0",
+ "Config": [{
+
+     "Name Bet Set": "1⃣ Mining Chance 81",
+     "Base Bet": "0.001",
+     "Max Bet": "OFF",
+     "Chance": "12",
+     "Random Chance": {
+          "Toggle": "ON",
+          "Min": "12",
+          "Max": "12"
+        },
+     "Bet": {
+          "Bet": "Low",
+          "Hi / Low": {
+              "Toggle": "ON",
+              "If Lose": "1",
+              "If Win": "1"
+            }
+        },
+     "If Win": "1",
+     "If Lose": "1.3",
+     "Reset If Win": "99",
+     "Reset If Profit": "0.00000001",
+     "Interval": "0"
